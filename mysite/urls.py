2c9b8a9d5eab1c741797e4978e4dd6eae0786383
Bruno Santos <48936982+brunosanntos@users.noreply.github.com>
2022-03-25 11:28:22 -0300

    Migrate models and page admin

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('polls/', include('tolls.urls')),
+    path('tolls/', include('tolls.urls')),
 ]
